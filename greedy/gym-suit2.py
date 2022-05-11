ee8f6d525bf0a668522c94e35f42ddf942ca55e7
Yebin <lyb2325@gmail.com>
2022-05-11 20:19:19 +0900

    gym-suit remove() 함수

--- greedy/gym-suit2.py
+++ greedy/gym-suit2.py
@@ -10,27 +10,28 @@ def solution(n, lost, reserve):
     answer = 0
     list=[] # 빌린 학생의 번호
     
+    # 오름차순 정렬
     lost.sort()
     reserve.sort()
     
-    # lost, reserve 중복 제거
+    # lost, reserve 중복으로 포함된 학생 제거
     for i in lost:
         if i in reserve:
             list.append(i)
-            del reserve[reserve.index(i)]
+            reserve.remove(i)
     
-    # 빌려줄 수 있는 학생
+    # 빌릴 수 있는 학생 제거
     for i in lost:
         # 앞번호에게 빌리기
         if i-1 in reserve:
             if i not in list:
                 list.append(i)
-                del reserve[reserve.index(i-1)]
+                reserve.remove(i-1)
         # 뒷번호에게 빌리기
         elif i+1 in reserve:
             if i not in list:
                 list.append(i)
-                del reserve[reserve.index(i+1)] 
+                reserve.remove(i+1) 
     
     answer = n-(len(lost)-len(list))
     return answer
