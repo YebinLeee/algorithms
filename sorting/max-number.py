ef9ad89146b960aa85a559317d2ca4eab2467b11
hojin <hojin@hojin.local>
2022-06-23 19:10:53 +0900

    fix ;

--- sorting/max-number.py
+++ sorting/max-number.py
@@ -4,16 +4,16 @@ def main():
     print(get_number([1,2,3,4,0]))
     print(get_number([6,10,2]))
     print(get_number([3, 30, 333, 303, 392]))
-    print(get_number([0,0,1]))
+    print(get_number([0,0]))
 
 def get_number(numbers):
     ns = [str(i)*3 for i in numbers]
     result = ""
     ns.sort(reverse=True)
     for n in ns:
-        for i in range(int(len(ns)/3)):
+        for i in range(int(len(n)/3)):
             result += n[i]
-    return result
+    return str(int(result))
 
 
 
