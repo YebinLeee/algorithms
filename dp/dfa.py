b6b5b9b93c90180ef4a8ba8e033d9e8e308f32d9
Yebin <lyb2325@gmail.com>
2022-07-07 17:46:05 +0900

    [프로그래머스 DP] N으로 표현하기

--- dp/dfa.py
+++ dp/dfa.py
@@ -1,35 +1,26 @@
 def solution(N, number):
-  dp = [[] for _ in range(9)] # N 사용 횟수에 대한 DP테이블
-  dp[1].append(N) # 1번 사용
-  
-  # 2번~8번 사용
-  for i in range(2, 9):
-    # i : number 사용 횟수
-    dp[i].append(int(str(N)*i)) # 5, 55, 555, (N을 이어붙인 수)
+    dp = [{N}]  # dp 리스트 (먼저 N을 삽입)
+    if N==number: 
+      return 1
+      
+    for i in range(1,9):
+        # i : number 사용 횟수
+        arr = [int(str(N)*(i+1))] # 5, 55, 555, (N을 이어붙인 수)
+        # dp[j]과 dp[i-j]에 대한 연산
+        for j in range(0, i//2+1):
+            for k in dp[j]:
+                for m in dp[i-j-1]:
+                    arr.append(k+m)
+                    arr.append(k-m)
+                    arr.append(m-k)
+                    arr.append(k*m)
+                    if m!=0:
+                        arr.append(k//m)
+                    if k!=0:
+                        arr.append(m//k)
+        dp.append(set(arr))
 
-    # dp[i-1]과 dp[i]에 대한 연산
-    for j in dp[i-1]:
-      # 덧셈
-      dp[i].append(j+N)
-      # 곱셈
-      dp[i].append(j*N)
-      # 나눗셈
-      dp[i].append(j//N)
-      if j>0:
-        dp[i].append(N//j)
-      # 뺄셈
-      dp[i].append(j-N)
-      dp[i].append(N-j)
-
-    if number in dp[i]:
-      # print(dp[i])
-      answer = i
-      return answer
-
-  answer = -1
-  return answer
-
-
-print(solution(5,12)) # 5로 12 만들기 ((55+5)/5)
-print(solution(2,11)) # 2로 11 만들기 ( 22/2 )
-print(solution(2, 111))
+        if number in set(arr):
+            return i+1
+        
+    return -1
